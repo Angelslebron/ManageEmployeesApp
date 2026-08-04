a273627c7298dfd00bd4b4a1cb1bcc3e0d574f93
Angelslebron <angellebronq17@gmail.com>
2026-08-03 21:41:49 -0400

    ME-4 implement authentication service

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI(
     title = "Manage Employees App",
     description = "This is a simple FastAPI application to manage employees.",
     version = "1.0.0"
 )
+
+app.add_middleware(SessionMiddleware, 
+secret_key="development-secret-key")
+
 
 @app.get("/")
 def home():
