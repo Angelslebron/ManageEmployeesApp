b764a7e258badb3a14dfdc6872278431d05ec3f5
Angelslebron <angellebronq17@gmail.com>
2026-08-05 21:19:02 -0400

    feat: make a html report and a few changes in the project.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,39 @@
 import os
 
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
-
     options = Options()
-
     options.add_argument("--start-maximized")
 
-    driver = webdriver.Chrome(
+    browser = webdriver.Chrome(
         options=options
     )
 
-    yield driver
+    yield browser
 
-    driver.quit()
+    browser.quit()
 
 
 @pytest.fixture
 def screenshot(driver):
-
     def capture(name):
-
         os.makedirs(
             "screenshots",
-            exist_ok=True
+            exist_ok=True,
         )
 
         path = os.path.join(
             "screenshots",
-            f"{name}.png"
+            f"{name}.png",
         )
 
         driver.save_screenshot(path)
+
+        return path
 
     return capture
